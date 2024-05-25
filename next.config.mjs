f126d9374aa99c451cfb783da8b2cf62e3cc1e92
10fotg <evo_reaction@hotmail.com>
2024-05-25 12:02:00 +0700

    redo www

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'https://healthypublicspaces.com/',
+        hostname: 'app-info.healthypublicspaces.com',
         port: '',
         pathname: '/images/**',
       },
