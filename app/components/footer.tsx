b56c94e7f8d8b2f5c0296870922c1846a1efa3c1
10fotg <evo_reaction@hotmail.com>
2024-06-02 14:54:36 +0700

    footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,22 @@
+// components/Footer.js
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-4 w-full fixed bottom-0">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} sdnthailand. All rights reserved.
+        </p>
+        <p>Power by Yellowdev</p>
+        <p className="text-sm">
+          Follow us on:
+          {/* <a href="https://twitter.com/yourcompany" className="ml-2 text-blue-400 hover:text-blue-600">Twitter</a>, */}
+          <a href="https://www.facebook.com/sdnthailand" className="ml-2 text-blue-400 hover:text-blue-600">Facebook</a>,
+          {/* <a href="https://instagram.com/yourcompany" className="ml-2 text-blue-400 hover:text-blue-600">Instagram</a> */}
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+  
---
+++ app/test/page.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { useEffect, useState } from 'react';
+interface Post {
+    id: number;
+    title: string;
+    imageUrl:string;
+    content: string;
+    categoryId: number;
+    createdAt: string;
+    updatedAt: string;
+  }
+  
+  interface ApiResponse {
+    posts: Post[];
+    totalPages: number;
+  }
+  
+const Posts: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('/api/posts');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data: ApiResponse = await response.json();
+        setPosts(data.posts);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <img src={`${post.imageUrl}`} alt='image'></img>
+            <p>{post.content}</p>
+            <small>Category ID: {post.categoryId}</small>
+            <br />
+            <small>Created at: {new Date(post.createdAt).toLocaleString()}</small>
+            <br />
+            <small>Updated at: {new Date(post.updatedAt).toLocaleString()}</small>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Posts;
