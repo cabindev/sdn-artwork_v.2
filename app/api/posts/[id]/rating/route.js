3ee8bfa78d97df5d302b595deaf5b124a0a9b439
10fotg <evo_reaction@hotmail.com>
2024-06-04 15:15:27 +0700

    add table ratings and route.js and modal/page.tsx

--- app/api/posts/[id]/rating/route.js
+++ app/api/posts/[id]/rating/route.js
@@ -1,4 +1,3 @@
-'use client';
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -35,52 +34,40 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-import { useEffect, useState } from 'react';
-var Posts = function () {
-    var _a = useState([]), posts = _a[0], setPosts = _a[1];
-    useEffect(function () {
-        var fetchPosts = function () { return __awaiter(void 0, void 0, void 0, function () {
-            var response, data, error_1;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        _a.trys.push([0, 3, , 4]);
-                        return [4 /*yield*/, fetch('/api/posts')];
-                    case 1:
-                        response = _a.sent();
-                        if (!response.ok) {
-                            throw new Error('Network response was not ok');
-                        }
-                        return [4 /*yield*/, response.json()];
-                    case 2:
-                        data = _a.sent();
-                        setPosts(data.posts);
-                        return [3 /*break*/, 4];
-                    case 3:
-                        error_1 = _a.sent();
-                        console.error('Fetch error:', error_1);
-                        return [3 /*break*/, 4];
-                    case 4: return [2 /*return*/];
-                }
-            });
-        }); };
-        fetchPosts();
-    }, []);
-    return (<div>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map(function (post) { return (<li key={post.id}>
-            <h2>{post.title}</h2>
-            <h2>{post.ratings}</h2>
-            <img src={"".concat(post.imageUrl)} alt='image'></img>
-            <p>{post.content}</p>
-            <small>Category ID: {post.categoryId}</small>
-            <br />
-            <small>Created at: {new Date(post.createdAt).toLocaleString()}</small>
-            <br />
-            <small>Updated at: {new Date(post.updatedAt).toLocaleString()}</small>
-          </li>); })}
-      </ul>
-    </div>);
-};
-export default Posts;
+// api/posts/[id]/rating/route.ts
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+var prisma = new PrismaClient();
+export function POST(req_1, _a) {
+    return __awaiter(this, arguments, void 0, function (req, _b) {
+        var post, updatedPost, error_1;
+        var params = _b.params;
+        return __generator(this, function (_c) {
+            switch (_c.label) {
+                case 0:
+                    _c.trys.push([0, 5, , 6]);
+                    return [4 /*yield*/, prisma.post.findUnique({
+                            where: { id: Number(params.id) },
+                        })];
+                case 1:
+                    post = _c.sent();
+                    if (!post) return [3 /*break*/, 3];
+                    return [4 /*yield*/, prisma.post.update({
+                            where: { id: Number(params.id) },
+                            data: {
+                                ratings: post.ratings + 1,
+                            },
+                        })];
+                case 2:
+                    updatedPost = _c.sent();
+                    return [2 /*return*/, NextResponse.json(updatedPost)];
+                case 3: return [2 /*return*/, NextResponse.json({ error: 'Post not found' }, { status: 404 })];
+                case 4: return [3 /*break*/, 6];
+                case 5:
+                    error_1 = _c.sent();
+                    return [2 /*return*/, NextResponse.json({ error: error_1 }, { status: 500 })];
+                case 6: return [2 /*return*/];
+            }
+        });
+    });
+}
